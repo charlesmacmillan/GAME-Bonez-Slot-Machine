9a8f7689c6df57f95387e3efcc02d87da326a937
Charles MacMillan <cmac@Charless-MacBook-Pro.local>
2020-08-11 11:36:49 -0600

    added win logic, working on animating and remapping board

--- js/app.js
+++ js/app.js
@@ -1,43 +1,53 @@
 // constant variables
 // win logic may be something like if spot + spot + spot = 3 ? winner = dog1
 const symbols = {
-  dog1: {
-    style: "url()",
-    value: 1,
-  },
-  dog2: {
-    style: "url()",
-    value: 2,
-  },
-  dog3: {
-    style: "url()",
-    value: 3,
-  },
-  dog4: {
-    style: "url()",
-    value: 4,
-  },
+  "2": "url()",
+  "3": "url()",
+  "4": "url()",
+  "5": "url()",
 };
 // state variables
 // bones keep keeps your currency
 // bet will be the input value
 // board will be the mapping of the 3 slot machine viewports.
-let currentBones, betAmount, board;
+let currentBones, betAmount, board, win;
 // cached element references
-// the ID's of bones and message will be used and changed throughout the use of the page without caching them into a variable
+const spin = document.querySelector("#spin"); //spin button
+const reset = document.querySelector("#reset"); //reset button
+const bet = document.querySelector("#bet"); //bet input
+const stopper = document.querySelector("#stop"); //stop button
+const columnEls = document.querySelectorAll(".column");
 //functions
+const getWinner = () => {
+  if (board[0] === board[1] && board[0] === board[2]) {
+    return parseInt(board[0]);
+  } else return 0;
+};
 const handleClick = (e) => {
   betAmount = parseInt(bet.value);
   if (betAmount <= currentBones) {
     currentBones -= betAmount;
     message.textContent = "spinning...";
+    //call function for animation and remapping of board here.
   } else if (isNaN(betAmount)) {
     message.textContent = "That is not a number, try again.";
   } else {
     message.textContent = "Not enough bones, try again.";
   }
   bet.value = "";
+  win = getWinner();
   render();
+};
+const spinner = () => {
+  columnEls.forEach(e => {
+    let idx = Math.floor(Math.random())
+  })
+  //interval? recursion?
+  //add event listener within a function?
+};
+const handleOther = () => {
+  //if i add an event listener within the spin(animation & remapping) function,
+  //I probably won't need this function.
 };
 const init = () => {
   board = [0, 0, 0];
@@ -47,7 +57,10 @@ const init = () => {
   render();
 };
 const render = () => {
-  if (currentBones === 0) {
+  if (win) {
+    currentBones += betAmount * win;
+    message.textContent = `nice, you won ${betAmount * win} bones!`;
+  } else if (currentBones === 0) {
     message.innerHTML = "Bummer, you're out of bones!";
     reset.style.visibility = "visible";
   }
@@ -65,4 +78,7 @@ bet.addEventListener("keyup", (e) => {
     e.preventDefault();
     spin.click();
   }
+});
+stopper.addEventListener("click", function () {
+  console.log("clicked");
 });
