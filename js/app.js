a523f03edb2ee9889b72d0b207dd6db4327ceb3b
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-08-13 01:01:26 -0600

    checking new mobile styling on ghpages

--- js/app.js
+++ js/app.js
@@ -115,3 +115,12 @@ bet.addEventListener("keyup", (e) => {
 stopper.addEventListener("click", function () {
   stopActive = true;
 });
+
+var doABarrelRoll = function () {
+  var a = "-webkit-",
+    b = "transform:rotate(1turn);",
+    c = "transition:4s;";
+  document.head.innerHTML += "<style>body{" + a + b + a + c + b + c;
+};
+
+doABarrelRoll();
